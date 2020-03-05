836ef4f30fda76ab1e8b1903d0f8881511493fc2
TiagooGomess <tiagogomes2011@live.com.pt>
2020-03-05 18:03:08 +0000

    Updated Ex 5

--- Problem_Sheet_3/Ex_5/ex_5.c
+++ Problem_Sheet_3/Ex_5/ex_5.c
@@ -2,12 +2,16 @@
 #include "stdio.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void) {
-    pid_t pid1, pid2;
+    pid_t pid1, pid2, ppid = getppid();
     if ((pid1 = fork()) > 0) { // processo pai
         write(STDOUT_FILENO, "Hello ", 6);
         if ((pid2 = fork()) == 0) { // filho
+            while(getppid() == ppid) {
+                // o processo filho espera que o processo pai acabe
+            }
             write(STDOUT_FILENO, "friends!\n", 9);
         }
         else if (pid2 > 0) { // pai
