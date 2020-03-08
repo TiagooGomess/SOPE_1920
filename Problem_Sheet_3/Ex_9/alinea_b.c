2ea6e64e2e6508fdeab8227732b2d863fc2699e2
TiagooGomess <tiagogomes2011@live.com.pt>
2020-03-08 17:38:46 +0000

    Updated Ex 9 b

--- Problem_Sheet_3/Ex_9/alinea_b.c
+++ Problem_Sheet_3/Ex_9/alinea_b.c
@@ -15,7 +15,13 @@ int main(int argc, char *argv[], char *envp[]) {
     if (pid > 0) {
         printf("My child is going to execute command \"ls -laR %s\"\n", argv[1]);
         wait(&status);
-        printf("\n\nMy child exited with exit %d\nWIFEXITED: %d\n\nWIFSIGNALED: %d\n\n", WEXITSTATUS(status), WIFEXITED(status), WIFSIGNALED(status));
+        printf("\n\nMy child exited with exit %d\n", WEXITSTATUS(status));
+        if (WIFEXITED(status)) {
+            printf("My child ended normally\n");
+        }
+        if (WIFSIGNALED(status)) {
+            printf("My child ended abnormally\n");
+        }
     
     }
     else if (pid == 0){
